65ba7c35ad78d2a17683581c5308328c19834895
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-13 13:08:54 +0200

    lection

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './css/styles.css';
 
 const refs = {
   menuList: document.querySelector('.menu__list'),
+  imageList: document.querySelector('main ul'),
 };
 
 const baseUrl = 'https://rickandmortyapi.com/api';
@@ -16,7 +17,7 @@ getDataServer(baseUrl).then(data => {
   const markup = Object.entries(data)
     .map(
       ([key, value]) => `
-    <li><a href="${value}>${key}</a></li>
+    <li><a href="${value}">${key}</a></li>
     `,
     )
     .join('');
@@ -26,5 +27,10 @@ getDataServer(baseUrl).then(data => {
 document.addEventListener('click', event => {
   if (event.target.nodeName !== 'A') return;
   event.preventDefault();
-  getDataServer(event.target.href).then(console.log);
+  getDataServer(event.target.href).then(data => {
+    const markupImage = data.results
+      .map(character => `<li class="image"><img src="${character.image}"></li>`)
+      .join('');
+    refs.imageList.innerHTML = markupImage;
+  });
 });
