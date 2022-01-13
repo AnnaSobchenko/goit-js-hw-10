81ccae43e89586cc0753bb84613876823eb05ff4
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-13 12:13:57 +0200

    create

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './css/styles.css';
+
+// const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  menuList: document.querySelector('.menu__list'),
+};
+
+const baseUrl = 'https://rickandmortyapi.com/api';
+
+function getDataServer(url) {
+  return fetch(url).then(response => response.json());
+}
+
+getDataServer(baseUrl).then(data => {
+  const markup = Object.entries(data)
+    .map(
+      ([key, value]) => `
+    <li><a href="${value}>${key}</a></li>
+    `,
+    )
+    .join('');
+  refs.menuList.innerHTML = markup;
+});
