2d4229cf15dc3aa12052f1116f6216c0bc62d54c
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-13 16:30:18 +0200

    hw

--- src/index.js
+++ src/index.js
@@ -1,36 +1,4 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
-// const DEBOUNCE_DELAY = 300;
-
-const refs = {
-  menuList: document.querySelector('.menu__list'),
-  imageList: document.querySelector('main ul'),
-};
-
-const baseUrl = 'https://rickandmortyapi.com/api';
-
-function getDataServer(url) {
-  return fetch(url).then(response => response.json());
-}
-
-getDataServer(baseUrl).then(data => {
-  const markup = Object.entries(data)
-    .map(
-      ([key, value]) => `
-    <li><a href="${value}">${key}</a></li>
-    `,
-    )
-    .join('');
-  refs.menuList.innerHTML = markup;
-});
-
-document.addEventListener('click', event => {
-  if (event.target.nodeName !== 'A') return;
-  event.preventDefault();
-  getDataServer(event.target.href).then(data => {
-    const markupImage = data.results
-      .map(character => `<li class="image"><img src="${character.image}"></li>`)
-      .join('');
-    refs.imageList.innerHTML = markupImage;
-  });
-});
+const DEBOUNCE_DELAY = 300;
