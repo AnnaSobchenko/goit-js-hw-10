fe5ce6462b91e573ad5462e14fefa553f31fc634
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-17 22:58:47 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -6,7 +6,7 @@ export default function fetchCountries(name) {
   return fetch(`${BASE_URL}/name/${name}`)
     .then(response => {
       if (!response.ok) {
-        throw new Error(Notiflix.Notify.failure(`${response.status}`));
+        throw new Error(Notiflix.Notify.failure(`Error ${response.status}`));
         // return Notiflix.Notify.failure('❌ Oops, there is no country with that name');
         // throw new Error(response.status);
       }
